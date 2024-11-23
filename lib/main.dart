1cbe87a2f7b4eb19de372799205991d426d6becd
projectteam2k24 <projectteam2k24.25@gmail.com>
2024-11-23 12:49:38 +0530

    date picker added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:migrantworker/contractor/screens/notification.dart';
+//import 'package:migrantworker/contractor/screens/notification.dart';
 //import 'package:migrantworker/worker/screens/edit_profile.dart';
-//import 'package:migrantworker/login.dart';
+import 'package:migrantworker/login.dart';
 
 
 void main() {
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false,home: ContractorNotifications()));
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false,home: LogIn()));
 }
